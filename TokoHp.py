04ca4c56a8ce21a898bde9b6d2ab0d14465ffc68
Firman Ahmad Bayumi <92419799+FirmanAhmadBayumi@users.noreply.github.com>
2022-10-23 20:17:34 +0700

    Update TokoHp.py

--- TokoHp.py
+++ TokoHp.py
@@ -5,6 +5,9 @@ def garis(jum):
     
 def min(payment):
     total = print('\nSisa uang anda saat ini Rp.', moneyBuyer - payment)
+    garis(60)
+    print('Terima kasih sudah melakukan transaksi di toko Atsalsuki!!!')
+    garis(60)
     return total
 
 moneyBuyer = 2000000
@@ -40,7 +43,7 @@ if passw == 'ATSALSUKI':
         for i in range (0, len(listMenu)):
             print(listMenu[i])
             
-        choose1 = int(input("\nSilahkan pilih menu yang tersedia (INPUT HARUS BERUPA ANGKA) : "))
+        choose1 = int(input('\nSilahkan pilih menu yang tersedia (INPUT HARUS BERUPA ANGKA) : '))
         choose1 != 1 or 2 or 3
         
         if choose1 == 1:
@@ -53,7 +56,7 @@ if passw == 'ATSALSUKI':
                 for i in range(0,len(listHpBaru)):
                     print(listHpBaru[i])
                     
-                choose2 = int(input("\nSilahkan pilih menu yang tersedia (INPUT HARUS BERUPA ANGKA) : "))
+                choose2 = int(input('\nSilahkan pilih menu yang tersedia (INPUT HARUS BERUPA ANGKA) : '))
                 choose2 != 1 or 2
                 
                 if choose2 == 1:
@@ -62,6 +65,12 @@ if passw == 'ATSALSUKI':
                     print('\nTotal uang yang anda miliki saat ini RP.',moneyBuyer)
                     payment1 = int(input('\nJumlah Hp yang akan dibeli: '))
                     payment1 = payment1 * sumsang
+                    
+                    if payment1 > 2000000:
+                        print('\nMaaf uang yang anda miliki saat ini tidak cukup!!!')
+                        print('\nSilahkan mengulang program dari awal!!!')
+                        exit()
+                        
                     print('\nTotal pembayaran sebesar Rp.',payment1)
                     min(payment1)
                        
@@ -71,6 +80,12 @@ if passw == 'ATSALSUKI':
                     print('\nTotal uang yang anda miliki saat ini RP.',moneyBuyer)
                     payment2 = int(input('Jumlah Hp yang akan dibeli: '))
                     payment2 = payment2 * appa
+                    
+                    if payment2 > 2000000:
+                        print('\nMaaf uang yang anda miliki saat ini tidak cukup!!!')
+                        print('\nSilahkan mengulang program dari awal!!!')
+                        exit()
+                    
                     print('Total pembayaran sebesar Rp.',payment2)
                     min(payment2)
                 
@@ -90,7 +105,7 @@ if passw == 'ATSALSUKI':
                 for i in range(0,len(listHpSeken)):
                     print(listHpSeken[i])
                     
-                choose3 = int(input("\nSilahkan pilih menu yang tersedia (INPUT HARUS BERUPA ANGKA) : "))
+                choose3 = int(input('\nSilahkan pilih menu yang tersedia (INPUT HARUS BERUPA ANGKA) : '))
                 choose3 != 1 or 2
                     
                 if choose3 == 1:
@@ -99,6 +114,12 @@ if passw == 'ATSALSUKI':
                     print('\nTotal uang yang anda miliki saat ini RP.',moneyBuyer)
                     payment3 = int(input('\nJumlah Hp yang akan dibeli: '))
                     payment3 = payment3 * linivi
+                    
+                    if payment3 > 2000000:
+                        print('\nMaaf uang yang anda miliki saat ini tidak cukup!!!')
+                        print('\nSilahkan mengulang program dari awal!!!')
+                        exit()
+                    
                     print('\nTotal pembayaran sebesar Rp.',payment3)
                     min(payment3)
 
@@ -126,7 +147,7 @@ if passw == 'ATSALSUKI':
                 for i in range(0,len(listServiceHp)):
                     print(listServiceHp[i])
                     
-                choose4 = int(input("\nSilahkan pilih menu yang tersedia (INPUT HARUS BERUPA ANGKA) : "))
+                choose4 = int(input('\nSilahkan pilih menu yang tersedia (INPUT HARUS BERUPA ANGKA) : '))
                 choose4 != 1 or 2
                 
                 if choose4 == 1:
@@ -135,6 +156,12 @@ if passw == 'ATSALSUKI':
                     print('\nTotal uang yang anda miliki saat ini RP.',moneyBuyer)
                     payment5 = int(input('\nJumlah Hp yang akan di service '))
                     payment5 = payment5 * servLinivi
+                    
+                    if payment5 > 2000000:
+                        print('\nMaaf uang yang anda miliki saat ini tidak cukup!!!')
+                        print('\nSilahkan mengulang program dari awal!!!')
+                        exit()
+                    
                     print('\nTotal pembayaran Rp.',payment5)
                     min(payment5)
                     
@@ -144,6 +171,12 @@ if passw == 'ATSALSUKI':
                     print('\nTotal uang yang anda miliki saat ini RP.',moneyBuyer)
                     payment6 = int(input('\nJumlah Hp yang akan di service '))
                     payment6 = payment6 * servPecce
+                    
+                    if payment6 > 2000000:
+                        print('\nMaaf uang yang anda miliki saat ini tidak cukup!!!')
+                        print('\nSilahkan mengulang program dari awal!!!')
+                        exit()
+                    
                     print('\nTotal pembayaran Rp.',payment6)
                     min(payment6)
                 
