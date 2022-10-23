2c64767eb345a58b7e2bfb5263cb8127bc6066a0
Firman Ahmad Bayumi <92419799+FirmanAhmadBayumi@users.noreply.github.com>
2022-10-23 19:55:35 +0700

    Update TokoHp.py

--- TokoHp.py
+++ TokoHp.py
@@ -1,3 +1,5 @@
+import os
+
 def garis(jum):
     print('=' * jum)
     
@@ -18,6 +20,8 @@ listHpBaru = ['\n1. Sumsang (Harga: RP. 500000)', '2. Appa (Harga: RP. 350000)']
 listHpSeken = ['\n1. Linivi (Harga: RP. 200000)', '2. Pecce (Harga: Rp. 250000)']
 listServiceHp = ['\n1. Linivi (Harga: Rp. 100000)', '2. Pecce (Harga: Rp. 150000)']
 
+
+os.system('cls')
 garis(65)
 print('\t\tSELAMAT DATANG DI TOKO HP ATSALSUKI')
 garis(65)
@@ -28,6 +32,7 @@ if passw == 'ATSALSUKI':
     print('Nama toko yang anda masukkan benar!!!\n')
 
     try :
+        os.system('cls')
         garis(55)
         print('\tBerikut daftar menu yang dapat anda pilih')
         garis(55)
@@ -40,7 +45,7 @@ if passw == 'ATSALSUKI':
         
         if choose1 == 1:
             try :
-                print('\n')
+                os.system('cls')
                 garis(65)
                 print('\tBerikut daftar Hp Baru yang dapat anda beli!!!')
                 garis(65)
@@ -52,6 +57,7 @@ if passw == 'ATSALSUKI':
                 choose2 != 1 or 2
                 
                 if choose2 == 1:
+                    os.system('cls')
                     print('\nAnda akan membeli Hp Sumsang dengan harga RP. 500000')
                     print('\nTotal uang yang anda miliki saat ini RP.',moneyBuyer)
                     payment1 = int(input('\nJumlah Hp yang akan dibeli: '))
@@ -60,6 +66,7 @@ if passw == 'ATSALSUKI':
                     min(payment1)
                        
                 elif choose2 == 2:
+                    os.system('cls')
                     print('\nAnda akan membeli Hp Appa dengan harga Rp. 350000')    
                     print('\nTotal uang yang anda miliki saat ini RP.',moneyBuyer)
                     payment2 = int(input('Jumlah Hp yang akan dibeli: '))
@@ -75,7 +82,7 @@ if passw == 'ATSALSUKI':
         elif choose1 == 2:
                     
             try:
-                print('\n')
+                os.system('cls')
                 garis(65)
                 print('\tBerikut daftar Hp Seken yang dapat anda beli!!!')
                 garis(65)
@@ -87,6 +94,7 @@ if passw == 'ATSALSUKI':
                 choose3 != 1 or 2
                     
                 if choose3 == 1:
+                    os.system('cls')
                     print('\nAnda akan membeli Hp Linivi dengan harga RP. 200000')
                     print('\nTotal uang yang anda miliki saat ini RP.',moneyBuyer)
                     payment3 = int(input('\nJumlah Hp yang akan dibeli: '))
@@ -96,6 +104,7 @@ if passw == 'ATSALSUKI':
 
                     
                 elif choose3 == 2:
+                    os.system('cls')
                     print('\nAnda akan membeli Hp Seken Pecce dengan harga Rp. 250000')
                     print('\nTotal uang yang anda miliki saat ini RP.',moneyBuyer)
                     payment4 = int(input('\nJumlah Hp yang akan dibeli: '))
@@ -109,7 +118,7 @@ if passw == 'ATSALSUKI':
         
         elif choose1 == 3:
             try:
-                print('\n')
+                os.system('cls')
                 garis(65)
                 print('\tBerikut daftar merk Hp yang dapat anda service!!!')
                 garis(65)
@@ -121,6 +130,7 @@ if passw == 'ATSALSUKI':
                 choose4 != 1 or 2
                 
                 if choose4 == 1:
+                    os.system('cls')
                     print('\nAnda akan menservice Hp Linivi dengan harga Rp. 100000')
                     print('\nTotal uang yang anda miliki saat ini RP.',moneyBuyer)
                     payment5 = int(input('\nJumlah Hp yang akan di service '))
@@ -129,6 +139,7 @@ if passw == 'ATSALSUKI':
                     min(payment5)
                     
                 elif choose4 == 2:
+                    os.system('cls')
                     print('\nAnda akan menservice Hp Pecce dengan harga Rp. 150000')
                     print('\nTotal uang yang anda miliki saat ini RP.',moneyBuyer)
                     payment6 = int(input('\nJumlah Hp yang akan di service '))
