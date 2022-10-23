c5915e5fd452baae1b8832673e3550eeafcb027d
Firman Ahmad Bayumi <92419799+FirmanAhmadBayumi@users.noreply.github.com>
2022-10-23 11:06:32 +0700

    Update TokoHp.py

--- TokoHp.py
+++ TokoHp.py
@@ -12,3 +12,17 @@ linivi = 200000
 pecce = 250000
 servLinivi = 100000
 servPecce = 150000
+
+listMenu = ['1. Beli Hp Baru','2. Beli Hp Seken', '3. Service Hp']
+listHpBaru = ['\n1. Sumsang (Harga: RP. 500000)', '2. Appa (Harga: RP. 350000)']
+listHpSeken = ['\n1. Linivi (Harga: RP. 200000)', '2. Pecce (Harga: Rp. 250000)']
+listServiceHp = ['\n1. Linivi (Harga: Rp. 100000)', '2. Pecce (Harga: Rp. 150000)']
+
+garis(65)
+print('\t\tSELAMAT DATANG DI TOKO HP ATSALSUKI')
+garis(65)
+
+passw = input('\nSilahkan masukkan nama toko ini untuk melanjutkan transaksi: ')
+
+if passw == 'ATSALSUKI':
+    print('Nama toko yang anda masukkan benar!!!\n')
