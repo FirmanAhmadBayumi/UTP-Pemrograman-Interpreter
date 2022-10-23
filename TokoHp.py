898236722142bc166b63f00389901f7de0c5165f
Firman Ahmad Bayumi <92419799+FirmanAhmadBayumi@users.noreply.github.com>
2022-10-23 11:05:21 +0700

    Update TokoHp.py

--- TokoHp.py
+++ TokoHp.py
@@ -4,3 +4,11 @@ def garis(jum):
 def min(payment):
     total = print('\nSisa uang anda saat ini Rp.', moneyBuyer - payment)
     return total
+
+moneyBuyer = 2000000
+sumsang = 500000
+appa = 350000
+linivi = 200000
+pecce = 250000
+servLinivi = 100000
+servPecce = 150000
